89ac71f799c3b80dcffd8368fb3de902d8fcda37
Hanna Lee <mani21na@gmail.com>
2020-02-05 04:15:19 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,6 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-    
-    redirect_to post_path(@post)
     if @post.valid?
       redirect_to post_path(@post)
     else
