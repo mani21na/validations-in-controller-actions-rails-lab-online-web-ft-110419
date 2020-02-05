523bef45ad2b7b3bc66a80a81e30e22e8457ab6e
Hanna Lee <mani21na@gmail.com>
2020-02-05 03:45:55 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,12 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    #@author = Author.create!(author_params)
+    @author = Author.new(author_params)
+    
+    if 
 
-    redirect_to author_path(@author)
+    #redirect_to author_path(@author)
   end
 
   private
