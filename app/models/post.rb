c696ac57fb551676d65e87a3b3610d4f0346609e
Hanna Lee <mani21na@gmail.com>
2020-02-05 04:10:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,3 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validates :content, length: { minimum: 100 }
 end
-
-  validates :title, presence: :true
-  validates :content, length: {minimum: 100}
-  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
